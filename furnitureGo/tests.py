b19045c278eaab56aa7dbdb51c661f32c90769ca
vicbett018 <zakayoochieng@gmail.com>
2022-11-02 22:28:57 +0300

    commiting changes

--- furnitureGo/tests.py
+++ furnitureGo/tests.py
@@ -3,11 +3,3 @@ from django.test import TestCase
 from furnitureGo.models import *
 
 # Create your tests here.
-class CreateUserTest(TestCase):
-    def setUp(self):
-        self.user1 = Customer.objects.create(
-        full_name = "Vic Bett",
-        address = "nairobi",
-        )
-    def test_product_created(self):
-        self.assertEquals(self.user1.full_name, "Vic Bett")

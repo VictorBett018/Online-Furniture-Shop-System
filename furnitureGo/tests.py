dd296acb4b7adec21b95cf7576b6be79620d0b8f
vicbett018 <zakayoochieng@gmail.com>
2022-11-09 13:28:40 +0300

    commiting changes

--- furnitureGo/tests.py
+++ furnitureGo/tests.py
@@ -1,5 +1,10 @@
 from django.test import TestCase
-
-from furnitureGo.models import *
+from .models import User
 
 # Create your tests here.
+class TestModel(TestCase):
+    def test_should_create_user(self):
+        user = User.objects.create_user(username ='username', email ='username@gmail.com')
+        user.set_password('password123')
+        user.save()
+        self.assertEqual(str(user), 'username')
